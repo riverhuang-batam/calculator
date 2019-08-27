bb1aee2f8d9b5ec75e5519e079eba522e9e754de
river <riverhuang011@gmail.com>
2019-08-27 12:59:18 +0700

    learning touchableopacity and putting number and operations

--- calculator/App.js
+++ calculator/App.js
@@ -1,42 +1,101 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+  import React, {Component} from 'react';
+  import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 
-export default class App extends Component{
-  constructor(){
-    super()
-  }
-  render(){
-  return (
-    <View style={styles.container}>
-      <View style={styles.result}></View>
-      <View style={styles.calculation}></View>
-      <View style={styles.buttons}>
-      <View style={styles.numbers}></View>
-      <View style={styles.operation}></View>
+  export default class App extends Component{
+    constructor(){
+      super()
+      this.state={}
+    }
+    render(){
+      let rows = []
+      let nums = [[1,2,3], [4,5,6], [7,8,9], [0,0,'=']]
+      
+      for(let i = 0; i < 4; i++){
+        let row = []
+        for(let j = 0; j < 3; j++){
+          row.push(
+            <TouchableOpacity style={styles.btn}>
+              <Text>{nums[i][j]}</Text>
+            </TouchableOpacity>
+          )
+          }
+          rows.push(<View style={styles.row}>{row}</View>)
+        }
+        
+
+        let ops = []
+        let operations = ['+','-','÷','x']
+        for(let i = 0; i < 4; i++){
+          ops.push(
+            <TouchableOpacity style={styles.btn}>
+              <Text>{operations[i]}</Text>
+            </TouchableOpacity>
+          )
+        }
+      
+    return (
+      <View style={styles.container}>
+        <View style={styles.result}>
+          <Text style={styles.resultText}>123</Text>
+        </View>
+        <View style={styles.calculation}>
+          <Text style={styles.calculationText}>123</Text>
+        </View>
+        <View style={styles.buttons}>
+        <View style={styles.numbers}>
+          {rows}
+        </View>
+        <View style={styles.operation}>
+          {ops}
+        </View>
+        </View>
       </View>
-    </View>
-    );
+      );
+    }
   }
-}
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  result:{
-    flex:1,
-    backgroundColor: 'red',
-  },
-  calculation:{
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },row:{
       flex:1,
-      backgroundColor:'blue',
-  },buttons:{
-    flex:7,
-    flexDirection:'row'
-  },numbers:{
-    flex:3,
-    backgroundColor:'black'
-  },operation:{
-    flex:1,
-    backgroundColor:'white'
-  }
-});
+      flexDirection:'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+    },resultText:{
+      fontSize:30,
+      color:'white'
+    },calculationText:{
+      fontSize:24,
+      color:'white'
+    },btn:{
+      flex:1,
+      alignItems:'center',
+      alignSelf: 'stretch',
+      justifyContent:'center'
+    },
+    result:{
+      flex:2,
+      backgroundColor: 'red',
+      justifyContent:'center',
+      alignItems:'flex-end'
+    },
+    calculation:{
+        flex:1,
+        backgroundColor:'blue',
+        justifyContent:'center',
+        alignItems:'flex-end'
+    },buttons:{
+      flex:7,
+      flexDirection:'row'
+    },numbers:{
+      flex:3,
+      backgroundColor:'white'
+    },operation:{
+      flex:1,
+      justifyContent:'space-around',
+      backgroundColor:'blue'
+    },btnText:{
+      color:'white',
+      fontSize: 20
+    }
+  });
